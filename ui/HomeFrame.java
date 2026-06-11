1c27bfdb899d9d2427c5cddcb57053bed0768914
Ganesh Gowda <ganeshabt180@gmail.com>
2026-06-11 20:04:22 +0530

    added ui folder

---
+++ ui/HomeFrame.java
@@ -0,0 +1,27 @@
+package ui;
+
+import javax.swing.*;
+
+public class HomeFrame extends JFrame {
+
+    public HomeFrame() {
+
+        setTitle("Library Management System");
+        setSize(400,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JButton addBookBtn = new JButton("Add Book");
+        addBookBtn.setBounds(120,50,150,40);
+
+        JButton exitBtn = new JButton("Exit");
+        exitBtn.setBounds(120,120,150,40);
+
+        add(addBookBtn);
+        add(exitBtn);
+
+        exitBtn.addActionListener(e -> System.exit(0));
+
+        setVisible(true);
+    }
+}
